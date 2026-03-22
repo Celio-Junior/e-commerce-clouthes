045a96767ce128affc8c380098c4b6a2d6b94cd2
Celio Junior <demon.junior13@gmail.com>
2026-03-22 20:54:58 -0300

    iniciando estilização do header admin e page de settings

--- src/components/admin/Header/Index.tsx
+++ src/components/admin/Header/Index.tsx
@@ -2,10 +2,22 @@ import clsx from 'clsx';
 
 export default function HeaderAdmin() {
   return (
-    <div className={clsx('w-full')}>
-      <h1 title="titulo/logo">NEXA</h1>
-
-      <div>settings</div>
+    <div className={clsx('flex justify-between items-center', 'px-10 my-15 w-full gap-2 ')}>
+      <h1 className="text-3xl" title="titulo/logo">
+        NEXA admin
+      </h1>
+      <nav
+        className={clsx(
+          'flex gap-2 text-gray-500 text-lg',
+          '[&>span]:cursor-pointer [&>span]:font-medium [&>span]:hover:text-gray-800 [&>span]:transition [&>span]:hover:scale-105',
+        )}
+      >
+        <span>overview</span>
+        <span>billboards</span>
+        <span>categories</span>
+        <span>settings</span>
+      </nav>
+      <div>User😎</div>
     </div>
   );
 }
